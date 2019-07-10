4c8c8b91a163f70fede7d779f891ee3ca9e8bb12
Zarsla <izalli@yahoo.com>
2019-07-10 06:24:06 -0400

    Update Zar_ParamRatePlus.js

--- Zar_ParamRatePlus.js
+++ Zar_ParamRatePlus.js
@@ -1,20 +1,16 @@
 /*:
 @name: Zarsla's Param Rate Plus
-@plugindesc v 1.00 Adds a function to alter the  Param Rate
+@plugindesc v 1.1 Adds a function to alter the  Param Rate
 @author Zarsla
-
 @help
-
 The Parameter Rate is the the value you
 see in the database, in the traits section,
 under param, under parameter. Where
 you can alter the base parameters
 by changing that percentacge value.
-
 This plugin gives you the ability, to
 manipulate that param rate just as
 you would flat base parameters in game.
-
 
 Parameter Ids
 0 - mhp (max hit points)
@@ -26,17 +22,43 @@ Parameter Ids
 6 - agi (agility)
 7 - luk (luck)
 
-
 Plugin Commands:
+
     IncreaseParamRateActor x y z
     Ex: IncreaseParamRateActor 1 0 10
     Increases parameter 0 rate, aka max hp,
     for actor 1 by 10%
 
+
     IncreaseParamRateMember x y z
     Ex: IncreaseParamRateMember 2 1 15
     Increases parameter rate 1, aka max mp,
     for the second party member by 15%
+
+    IncreaseParamRatebyVariableActor x y z
+    Ex: IncreaseParamRateActor 1 0 10
+    Increases parameter 0 rate, aka max hp,
+    for actor 1 by the amount of variable 10
+    Make sure that variable amounts are constant
+    numbers, ie variable 10 is set to say
+    5, to increase in 5%
+
+    Remember that the value in variable z is what 
+    the param rate will change by, 
+    for example variable z has the value of 1, 
+    then the param rate will change by 1%
+
+    IncreaseParamRatebyVariableMember x y z
+    Ex: IncreaseParamRateMember 2 1 15
+    Increases parameter rate 1, aka max mp,
+    for the second party member by the amount of
+    variable 15
+
+    Remember that the value in variable z is what 
+    the param rate will change by, 
+    for example variable z has the value of 1, 
+    then the param rate will change by 1%
+
 
     DecreaseParamRateActor x y z
     Ex: DecreaseParamRateActor 3 2 5
@@ -46,24 +68,43 @@ Plugin Commands:
     DecreaseParamRateMember x y z
     Ex: DecreaseParamRateMember 4 4 20
     Decreases parameter rate 4, aka defense rate, 
-    for the fourth party member by 20%		
+    for the fourth party member by 20%
+
+    DecreaseParamRatebyVariableActor x y z
+    Ex: DecreaseParamRateActor 3 2 5
+    Decreases parameter rate 2, aka attack rate,
+    for actor 3 by the amount of variable 5
+
+    Remember that the value in variable z is what 
+    the param rate will change by, 
+    for example variable z has the value of 1, 
+    then the param rate will change by 1%
+
+    DecreaseParamRatebyVariableMember x y z
+    Ex: DecreaseParamRateMember 4 4 20
+    Decreases parameter rate 4, aka defense rate, 
+    for the fourth party member by the amount of
+    variable 20      
+
+    Remember that the value in variable z is what 
+    the param rate will change by, 
+    for example variable z has the value of 1, 
+    then the param rate will change by 1%
 
 Script Calls:
 
 Params Rate Plus:
-        battler.paramRatePlus(paramId)
-
+        battler.paramRatePlus(paramId)a
         where battler is the actor or enemy.
         where paramId is the Parameter Id
         where value is the amount being added,
         to decrease a value use -value.
         Also use decimal values for the value, 
         ie 0.10, for 10%
-
         This will return the parameter rate
         plus for parameter parameId
 
-Add Param Rate
+Add Param Rate:
         battler.addParamRate(paramId, value)
         
         where battler is the actor or enemy.
@@ -72,22 +113,19 @@ Add Param Rate
         to decrease a value use -value.
         Also use decimal values for the value, 
         ie 0.10, for 10%
-
         This will change the parameter rate
         for parameter parameId, by value 
         amount.
 
-Clears Param Rate Plus
-        
+Clears Param Rate Plus:        
         battler.clearParamRatePlus()
-
         where battler is the actor or enemy.    
-
         refresesh/clears all the ParamRatePlus.
 
 Changelog
+  v 1.1 Added the ability to let manipulate
+  param rate with variables.
   v 1.0 plugin created!
-
 */
 
 var JSONSuperParse = function (string) {
@@ -117,19 +155,29 @@ Game_Interpreter.prototype.pluginCommand = function(command, args) {
     ZPRP.Game_Interpreter_prototype_pluginCommand.call(this, command, args);
     // to be overridden by plugins
     if(command === 'IncreaseParamRateActor'){
-    		args[2] *= 0.01;
-    		$gameActors.actor(args[0]).addParamRate(args[1], args[2]);
+            args[2] *= 0.01;
+            $gameActors.actor(args[0]).addParamRate(args[1], args[2]);
     } else if(command === 'IncreaseParamRateMember'){
-    		args[0] -= 1;
-    		args[2] *= 0.01;
-    		$gameParty.members()[args[0]].addParamRate(args[1], args[2]);
+            args[0] -= 1;
+            args[2] *= 0.01;
+            $gameParty.members()[args[0]].addParamRate(args[1], args[2]);
     } else if(command === 'DecreaseParamRateActor'){
-    		args[2] *= -0.01;
-    		$gameActors.actor(args[0]).addParamRate(args[1], args[2]);
+            args[2] *= -0.01;
+            $gameActors.actor(args[0]).addParamRate(args[1], args[2]);
     } else if(command === 'DecreaseParamRateMember'){
-    		args[0] -= 1;
-    		args[2] *= -0.01;
-    		$gameParty.members()[args[0]].addParamRate(args[1], args[2]);
+            args[0] -= 1;
+            args[2] *= -0.01;
+            $gameParty.members()[args[0]].addParamRate(args[1], args[2]);
+    } else if(command === 'IncreaseParamRatebyVariableActor'){
+            $gameActors.actor(args[0]).addParamRate(args[1], $gameVariables.value(args[2])*0.01);
+    } else if(command === 'IncreaseParamRatebyVariableMember'){
+            args[0] -= 1;
+            $gameParty.members()[args[0]].addParamRate(args[1], $gameVariables.value(args[2])*0.01);
+    } else if(command === 'DecreaseParamRateVariableActor'){
+            $gameActors.actor(args[0]).addParamRate(args[1], -$gameVariables.value(args[2])*0.01);
+    } else if(command === 'DecreaseParamRateVariableMember'){
+            args[0] -= 1;
+            $gameParty.members()[args[0]].addParamRate(args[1], -$gameVariables.value(args[2] * 0.01);
     }
 };
 
@@ -146,8 +194,8 @@ Game_BattlerBase.prototype.clearParamRatePlus = function() {
 
 ZPRP.Game_BattlerBase_prototype_paramRate = Game_BattlerBase.prototype.paramRate;
 Game_BattlerBase.prototype.paramRate = function(paramId) {
-	var amount = ZPRP.Game_BattlerBase_prototype_paramRate.call(this, paramId);
-	amount += this.paramRatePlus(paramId);
+    var amount = ZPRP.Game_BattlerBase_prototype_paramRate.call(this, paramId);
+    amount += this.paramRatePlus(paramId);
     return amount; 
 };
 
